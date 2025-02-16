bce03b70f65c56bb6fddef6c0f7b38005f350b30
Prakhar Awasthi <prakharaawasthi41509@gmail.com>
2025-02-16 18:41:15 +0530

    commit

--- frontend/src/layouts/app-layout.jsx
+++ frontend/src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
+        Made for Shramiks ❤️
       </div>
     </div>
   );
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -21,18 +21,12 @@ const LandingPage = () => {
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center ">
         <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
-          Find Your Dream Job
           <span className="flex items-center gap-2 sm:gap-6">
-            and get
-            <img
-              src="/logo.png"
-              className="h-14 sm:h-24 lg:h-32"
-              alt="Hirrd Logo"
-            />
+          Empowering Labor, Building Futures.
           </span>
         </h1>
         <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
-          Explore thousands of job listings or find the perfect candidate
+          Explore thousands of job listings or find the perfect work force
         </p>
       </section>
       <div className="flex gap-6 justify-center">
@@ -47,15 +41,15 @@ const LandingPage = () => {
           </Button>
         </Link>
       </div>
-      <Carousel
+      {/* <Carousel
         plugins={[
           Autoplay({
             delay: 2000,
           }),
         ]}
         className="w-full py-10"
-      >
-        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+      > */}
+        {/* <CarouselContent className="flex gap-5 sm:gap-20 items-center">
           {companies.map(({ name, id, path }) => (
             <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
               <img
@@ -66,17 +60,17 @@ const LandingPage = () => {
             </CarouselItem>
           ))}
         </CarouselContent>
-      </Carousel>
+      </Carousel> */}
 
       <img src="/banner.jpeg" className="w-full" />
 
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <Card>
           <CardHeader>
-            <CardTitle className="font-bold">For Job Seekers</CardTitle>
+            <CardTitle className="font-bold">For Workers</CardTitle>
           </CardHeader>
           <CardContent>
-            Search and apply for jobs, track applications, and more.
+            Search and apply for jobs, track status and more.
           </CardContent>
         </Card>
         <Card>
@@ -84,7 +78,7 @@ const LandingPage = () => {
             <CardTitle className="font-bold">For Employers</CardTitle>
           </CardHeader>
           <CardContent>
-            Post jobs, manage applications, and find the best candidates.
+            Post jobs, manage applications, and find the best work force.
           </CardContent>
         </Card>
       </section>
